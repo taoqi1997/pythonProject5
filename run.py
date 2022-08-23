5c4bbf5ace90e7cdd67d4c503886bbe5649697f5
taoqi1997 <1454701957@qq.com>
2022-08-23 18:10:29 +0800

    最新

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import sys
-
-sys.path.append("..")
-
+import os
+from common.Handle_path import HOST_PATH
+sys.path.append(os.path.join(HOST_PATH,"run.py"))
 import unittest
 from unittestreport import TestRunner
 from common.Handle_path import TEST_CASE
+
+
 
 test = unittest.defaultTestLoader.discover(TEST_CASE)
 runner = TestRunner(test,title="接口测试报告",tester="taoqi")
